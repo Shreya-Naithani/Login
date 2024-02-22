639ae0685361a392abf2757954ce03182a40b3d5
Shreya-Naithani <102851285+Shreya-Naithani@users.noreply.github.com>
2024-02-22 20:37:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from './Component/Home';
 import Login from './Component/Login';
 import Register from './Component/Register';
+import User from './Component/User';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/" element={<Register />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/home" element={<Home />}></Route>
+      <Route path="/user" element={<User />}></Route>
     </Routes>
   </BrowserRouter>;
   </div>
